eb3ee0479b7ae10c437bb8727b2b2be10c9753bd
David Kinzer <dtkinzer@gmail.com>
2023-11-15 17:31:30 -0500

    BL-864: Allow logging for Primo::Search class via config.
    Also allows debugging of Primo requests via configuration.

--- lib/primo/config.rb
+++ lib/primo/config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "logger"
+
 module Primo
   class << self
     attr_accessor :configuration
@@ -8,12 +10,35 @@ module Primo
   def self.configure()
     self.configuration ||= Configuration.new
     yield(configuration) if block_given?
+    on_configure
+  end
+
+  def self.on_configure
+    _configure_logging
+    _configure_debugging
+  end
+
+
+  def self._configure_logging
+    if configuration.enable_log_requests
+      primo_logger = configuration.logger
+      log_level = Primo.configuration.log_level
+      log_format = Primo.configuration.log_format
+      Primo::Search.logger primo_logger, log_level, log_format
+    end
+  end
+
+  def self._configure_debugging
+    if configuration.enable_debug_output
+      Primo::Search.debug_output configuration.debug_output_stream
+    end
   end
 
   class Configuration
     attr_accessor :apikey, :region, :operator, :field, :precision
     attr_accessor :context, :environment, :inst, :vid, :scope, :pcavailability, :enable_loggable
-    attr_accessor :timeout, :validate_parameters
+    attr_accessor :timeout, :validate_parameters, :enable_log_requests, :enable_debug_output
+    attr_accessor :logger, :log_level, :log_format, :debug_output_stream
 
     def initialize
       @apikey = "TEST_API_KEY"
@@ -28,6 +53,13 @@ module Primo
       @enable_loggable = false
       @timeout = 5
       @validate_parameters = true
+      @enable_log_requests = false
+      # debug_output should only be enabled in development mode.
+      @enable_debug_output = false
+      @logger = Logger.new("log/primo_requests.log")
+      @log_level = :info
+      @log_format = :logstash
+      @debug_output_stream = $stderr
     end
 
     def timeout(params = {})
